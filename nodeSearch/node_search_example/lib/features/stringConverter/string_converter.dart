be218bf250bfe1d7a6d4b0c8cdaac8447bfeeab5
Tim <tijak@protonmail.ch>
2020-10-14 11:45:16 +0100

    change var names

--- nodeSearch/node_search_example/lib/features/stringConverter/string_converter.dart
+++ nodeSearch/node_search_example/lib/features/stringConverter/string_converter.dart
@@ -2,17 +2,17 @@ import 'package:node_search_example/features/nodesearch/date_time_converter.dart
 
 class StringConverter {
   String convertJson(String jsonString, TranslationDirection direction) {
-    List<String> stringArr = jsonString.split("\"");
-    List<String> newStringArr = new List();
-    for(String each in stringArr) {
+    List<String> jsonCharArray = jsonString.split("\"");
+    List<String> convertedArray = new List();
+    for(String each in jsonCharArray) {
       each = each.trim();
       if (DateTime.tryParse(each) != null) {
         each = DateTimeConverter.convertToCleanString(each, direction);
       }
-      newStringArr.add(each);
+      convertedArray.add(each);
     }
     StringBuffer convertedString = StringBuffer();//newStringArr.toString();
-    for (String string in newStringArr) {
+    for (String string in convertedArray) {
       // only quote strings not integers or []/{}
       String trimmedString = string.trim();
       if (!(trimmedString.contains("[") || trimmedString.contains("]") || trimmedString.contains("{") || trimmedString.contains("}") || trimmedString.contains(":") || trimmedString.contains(","))) {
